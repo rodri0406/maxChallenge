6a2e7fcfe77ed0f9d8b9faaaf6d8ffa7fb345172
Rodrigo Medina <rmedina@technisys.com>
2021-11-21 15:43:41 -0300

    Se creo proyecto base

---
+++ src/main/java/com/rmedina/max/challenge/app/MaxChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.rmedina.max.challenge.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmedina/max/challenge/app/MaxChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmedina.max.challenge.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
